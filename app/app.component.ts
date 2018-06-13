5c7da4003532704b8411b23836a413e822925b96
James Tarkenton <james.tarkenton@gmail.com>
2018-06-13 17:17:56 -0400

    Initial version.

---
+++ app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<img src="https://i.imgur.com/DXRVK.jpg" alt="Allan please add details"> '
+})
+
+export class AppComponent { }
